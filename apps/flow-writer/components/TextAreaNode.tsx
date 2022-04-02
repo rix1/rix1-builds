dd16519bdf9964c15bd52d981f5fd448d9fc27fb
Rikard Eide <rikardeide@gmail.com>
2022-04-02 09:34:42 +0200

    fw: add basic text editor, state management (not used) and saving controls

--- apps/flow-writer/components/TextAreaNode.tsx
+++ apps/flow-writer/components/TextAreaNode.tsx
@@ -1,6 +1,12 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Handle, Position } from 'react-flow-renderer';
+import { CheckIcon } from '@heroicons/react/outline';
+
 import usePortal from 'react-useportal';
+import debounce from '../lib/debuounce';
+import { useStore } from '../lib/store';
+import clsx from 'clsx';
+import dayjs from 'dayjs';
 
 type TextAreaNodeProps = {
   data: {
@@ -8,21 +14,26 @@ type TextAreaNodeProps = {
   };
 };
 
-function TextAreaNode({ data }: TextAreaNodeProps) {
+function TextAreaNode({ data, id }: TextAreaNodeProps) {
+  const nodeData = useStore((state) => state.nodeContent);
+  const [savedAt, setSavedAt] = useState(null);
+  const [isWriting, setIsWriting] = useState(false);
+
   const { ref, openPortal, closePortal, isOpen, Portal } = usePortal({
     bindTo: document && document.getElementById('editor-root'),
-    // onOpen({ portal }) {
-    //   portal.current.className =
-    //     'fixed right-0 top-0 bottom-0 z-10 h-full w-3/5 bg-slate-100';
-    // },
-    // onClose({ portal }) {
-    //   portal.current.className = '';
-    // },
   });
 
-  const onChange = useCallback((evt) => {
-    // console.log(evt.target.value);
-  }, []);
+  useEffect(() => {
+    console.log('isWriting', isWriting);
+  }, [isWriting]);
+
+  const debouncedOnChange = useCallback(
+    debounce((evt) => {
+      setIsWriting(false);
+      setSavedAt(new Date().getTime());
+    }, 500),
+    [],
+  );
 
   const handleEdit = useCallback((evt) => {
     openPortal();
@@ -41,7 +52,6 @@ function TextAreaNode({ data }: TextAreaNodeProps) {
           className="h-32 w-48 bg-slate-50 p-3 text-slate-600"
           disabled
           name="text"
-          onChange={onChange}
         />
         <div className="text-right">
           <button ref={ref} className="mt-2" onClick={handleEdit}>
@@ -49,8 +59,25 @@ function TextAreaNode({ data }: TextAreaNodeProps) {
           </button>
         </div>
         {isOpen && (
-          <Portal className="h-full w-full bg-red-300">
-            <textarea className="fixed top-0 right-0 bottom-0 w-1/2 resize-none bg-slate-100 p-3 shadow-xl ring-1 ring-slate-200" />
+          <Portal className="relative h-full w-full bg-red-300">
+            <div className="nodrag fixed top-0 right-0 bottom-0 w-1/2 bg-slate-100 shadow-xl ring-1 ring-slate-200 ">
+              <span
+                className={clsx(
+                  'absolute bottom-1 left-1 flex text-xs text-slate-400 transition duration-300 ease-in-out',
+                  isWriting ? 'opacity-0' : 'opacity-100',
+                )}
+              >
+                <CheckIcon className="mr-1 h-4 w-4" /> Saved{' '}
+                {dayjs(savedAt).fromNow()}
+              </span>
+              <textarea
+                onChange={() => {
+                  setIsWriting(true);
+                  debouncedOnChange();
+                }}
+                className="h-full w-full resize-none bg-transparent p-3"
+              />
+            </div>
           </Portal>
         )}
       </div>
---
+++ apps/flow-writer/lib/store.ts
@@ -0,0 +1,94 @@
+import create, { GetState, SetState } from 'zustand';
+
+enum NodeType {
+  Inuput = 'input',
+  Output = 'output',
+  TextArea = 'textArea',
+}
+
+const initialNodes = {
+  '1': {
+    id: '1',
+    type: NodeType.Inuput,
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+
+  '2': {
+    id: '2',
+    type: NodeType.TextArea,
+    // you can also pass a React component as a label
+    data: { label: 'rikard' },
+    position: { x: 100, y: 125 },
+  },
+  '3': {
+    id: '3',
+    type: NodeType.Output,
+    data: { label: 'Output Node' },
+    position: { x: 450, y: 450 },
+  },
+};
+
+const nodeIds = ['1', '2', '3'];
+
+export type StoreSlice<T extends object, E extends object = T> = (
+  set: SetState<E extends T ? E : E & T>,
+  get: GetState<E extends T ? E : E & T>,
+) => T;
+
+interface INodeActionsSlice {
+  addNode: (id, FlowNode) => void;
+  updateNode: (id, FlowNode) => void;
+}
+
+const flowNodeActionsSlice: StoreSlice<INodeActionsSlice, INodeSlice> = (
+  set,
+  get,
+) => ({
+  addNode: (id, node) => set((prev) => ({ ...prev, [id]: node })),
+  updateNode: (id, updatedFields) =>
+    set((prev) => ({ ...prev, [id]: { ...prev[id], updatedFields } })),
+});
+
+type FlowNode = {
+  id: string;
+  type: NodeType;
+  data: {
+    label: string;
+  };
+  position: { x: number; y: number };
+};
+
+interface INodeSlice {
+  byId: {
+    [key: string]: FlowNode;
+  };
+  allIds: string[];
+}
+
+const flowNodeSlice: StoreSlice<INodeSlice> = (set, get) => ({
+  byId: initialNodes,
+  allIds: nodeIds,
+});
+
+type ContentShape = {
+  [key: string]: {
+    raw: string;
+  };
+};
+
+interface INodeContentSlice {
+  nodeContent: ContentShape;
+}
+
+const nodeContentSlice: StoreSlice<INodeContentSlice> = (set, get) => ({
+  nodeContent: {},
+});
+
+const createRootSlice = (set: SetState<any>, get: GetState<any>) => ({
+  ...flowNodeActionsSlice(set, get),
+  ...flowNodeSlice(set, get),
+  ...nodeContentSlice(set, get),
+});
+
+export const useStore = create(createRootSlice);
