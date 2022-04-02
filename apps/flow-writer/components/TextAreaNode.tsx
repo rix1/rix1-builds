be19ac10aea7a07f43526e52a9ac1b4b4b3544cf
Rikard Eide <rikardeide@gmail.com>
2022-04-03 00:52:59 +0200

    fw: add editor state and markdown preview functionality

--- apps/flow-writer/components/TextAreaNode.tsx
+++ apps/flow-writer/components/TextAreaNode.tsx
@@ -1,81 +1,118 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Handle, Position } from 'react-flow-renderer';
-import { CheckIcon } from '@heroicons/react/outline';
+import { CheckIcon, PaperClipIcon } from '@heroicons/react/outline';
 
 import usePortal from 'react-useportal';
 import debounce from '../lib/debuounce';
 import { useStore } from '../lib/store';
 import clsx from 'clsx';
 import dayjs from 'dayjs';
+import SaveIndicator from './SaveIndicator';
+import Editor from './Editor';
 
 type TextAreaNodeProps = {
   data: {
     label: string;
   };
+  id: string;
 };
 
-function TextAreaNode({ data, id }: TextAreaNodeProps) {
-  const nodeData = useStore((state) => state.nodeContent);
+function TextAreaNode({ data, id, ...rest }: TextAreaNodeProps) {
+  const content = useStore((state) => state.getContent(id));
+  const setContent = useStore((store) => store.setContent);
   const [savedAt, setSavedAt] = useState(null);
   const [isWriting, setIsWriting] = useState(false);
+  const contentRef = useRef<HTMLTextAreaElement>();
 
   const { ref, openPortal, closePortal, isOpen, Portal } = usePortal({
     bindTo: document && document.getElementById('editor-root'),
   });
 
-  useEffect(() => {
-    console.log('isWriting', isWriting);
-  }, [isWriting]);
-
   const debouncedOnChange = useCallback(
-    debounce((evt) => {
+    debounce((event) => {
       setIsWriting(false);
       setSavedAt(new Date().getTime());
-    }, 500),
+      // if (contentRef.current?.value) {
+      setContent(id, { raw: contentRef.current.value });
+      // }
+    }, 300),
     [],
   );
 
-  const handleEdit = useCallback((evt) => {
-    openPortal();
-    console.log('should edit');
+  const handleContentChange = useCallback((event) => {
+    setIsWriting(true);
   }, []);
+
+  const openEditor = useCallback((evt) => {
+    openPortal();
+  }, []);
+
+  const { xPos, yPos, type, selected } = rest;
 
   return (
     <>
       <Handle type="target" position={Position.Top} />
-      <div className="rounded-md bg-white p-3 shadow-md ring-1 ring-slate-200">
-        <label className="sr-only block text-left" htmlFor="text">
-          Text:
-        </label>
-        <textarea
-          id="text"
-          className="h-32 w-48 bg-slate-50 p-3 text-slate-600"
-          disabled
-          name="text"
-        />
+      <div
+        className={clsx(
+          'rounded-md bg-white text-left shadow-md ring-1 ring-slate-200',
+          selected && 'ring-slate-500',
+        )}
+      >
+        <div className="overflow-hidden">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Node {id}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">{}</p>
+          </div>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Position</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  <code>
+                    x: {Math.round(xPos)} y: {Math.round(yPos)}
+                  </code>
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Last updated
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {savedAt ? dayjs(savedAt).fromNow() : 'Not saved yet'}
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Content length
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {content.raw.length} characters
+                </dd>
+              </div>
+              <div className="border-t bg-white px-4 py-5 italic sm:px-6">
+                <p>{content?.raw.slice(0, 50)}...</p>
+              </div>
+            </dl>
+          </div>
+        </div>
         <div className="text-right">
-          <button ref={ref} className="mt-2" onClick={handleEdit}>
+          <button ref={ref} className="mt-4 mr-4 mb-4" onClick={openEditor}>
             Edit
           </button>
         </div>
         {isOpen && (
           <Portal className="relative h-full w-full bg-red-300">
             <div className="nodrag fixed top-0 right-0 bottom-0 w-1/2 bg-slate-100 shadow-xl ring-1 ring-slate-200 ">
-              <span
-                className={clsx(
-                  'absolute bottom-1 left-1 flex text-xs text-slate-400 transition duration-300 ease-in-out',
-                  isWriting ? 'opacity-0' : 'opacity-100',
-                )}
-              >
-                <CheckIcon className="mr-1 h-4 w-4" /> Saved{' '}
-                {dayjs(savedAt).fromNow()}
-              </span>
-              <textarea
-                onChange={() => {
-                  setIsWriting(true);
-                  debouncedOnChange();
+              <SaveIndicator shouldHide={isWriting} lastSavedAt={savedAt} />
+              <Editor
+                ref={contentRef}
+                defaultValue={content.raw}
+                onChange={(event) => {
+                  handleContentChange(event);
+                  debouncedOnChange(event);
                 }}
-                className="h-full w-full resize-none bg-transparent p-3"
               />
             </div>
           </Portal>
