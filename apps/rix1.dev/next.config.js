b9c636ee87ba0ace0c03b2a6198e14016233a079
Rikard Eide <rikardeide@gmail.com>
2022-05-22 00:06:02 +0200

    rix1.dev: set up homepage and port content from relativity

--- apps/rix1.dev/next.config.js
+++ apps/rix1.dev/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require('next-transpile-modules')(['ui']);
+const withContentlayer = require('next-contentlayer').withContentlayer;
 
-module.exports = withTM({
-  reactStrictMode: true,
-});
+module.exports = withTM(
+  withContentlayer({
+    reactStrictMode: true,
+  }),
+);
