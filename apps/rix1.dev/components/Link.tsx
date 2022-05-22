4ba498556c758f024c10cd917cf11cbd04002c0b
Rikard Eide <rikardeide@gmail.com>
2022-05-22 12:02:47 +0200

    rix1.dev: visual improvements on home page

--- apps/rix1.dev/components/Link.tsx
+++ apps/rix1.dev/components/Link.tsx
@@ -6,23 +6,20 @@ type LinkProps = {
 };
 
 const Link = ({ children, href }: LinkProps) => {
+  const classNames =
+    'bg-gradient-to-r from-[#EA5634] to-[#db2777] bg-clip-text text-transparent hover:from-[#db2777] hover:to-purple-900';
+
   const isAbsolute = href.startsWith('http');
   if (isAbsolute) {
     return (
-      <a
-        href={href}
-        target="_blank"
-        className="bg-gradient-to-r from-[#EA5634] to-[#db2777] bg-clip-text text-transparent"
-      >
+      <a href={href} target="_blank" className={classNames} rel="noreferrer">
         {children}
       </a>
     );
   }
   return (
     <NextLink href={href}>
-      <a className="bg-gradient-to-r from-[#EA5634] to-[#db2777] bg-clip-text text-transparent">
-        {children}
-      </a>
+      <a className={classNames}>{children}</a>
     </NextLink>
   );
 };
--- apps/rix1.dev/components/ProjectSection.tsx
+++ apps/rix1.dev/components/ProjectSection.tsx
@@ -53,40 +53,42 @@ const projects = [
   },
 ];
 
-type ProjectSectionProps = {
-  // children: React.ReactNode;
-};
+type ProjectSectionProps = {};
 
 const ProjectSection = ({}: ProjectSectionProps) => {
   return (
     <>
       <h2 className="mb-8 text-center text-3xl">Sometimes I make stuff:</h2>
-      <div className="grid grid-cols-2 gap-4 text-lg md:grid-cols-3">
+      <div className="grid grid-flow-row grid-cols-2 gap-6 text-lg md:grid-cols-3 lg:grid-rows-3">
         {projects.map((project) => (
-          <article key={project.title} className="">
+          <article key={project.title} className="max-w-xs">
             <a href={project.link} className="">
               <h3 className={['', project.link && 'underline'].join(' ')}>
                 {project.title}
               </h3>
             </a>
-            <h3 className="">{project.description} </h3>
+            <h3 className="font-normal text-slate-700">
+              {project.description}{' '}
+            </h3>
             {project.repo && (
-              <Link href={project.repo}>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#EA5634"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="mr-2 inline-block align-middle"
-                >
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22" />
-                </svg>
-                Repository
-              </Link>
+              <div className="self-end">
+                <Link href={project.repo}>
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#EA5634"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2 inline-block align-middle"
+                  >
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22" />
+                  </svg>
+                  Repository
+                </Link>
+              </div>
             )}
           </article>
         ))}
