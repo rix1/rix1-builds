f8dafcf3426e64c471076c32ce80c6a1876050b4
Rikard Eide <rikardeide@gmail.com>
2022-07-21 10:00:41 +0200

    CP: format project with prettier

--- apps/cabin-planner/src/pages/index.tsx
+++ apps/cabin-planner/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { trpc } from "../utils/trpc";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { trpc } from '../utils/trpc';
 
 type TechnologyCardProps = {
   name: string;
@@ -9,7 +9,7 @@ type TechnologyCardProps = {
 };
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
 
   return (
     <>
