65b1e51683bcde6937fcb30d72b7e7751f3e1184
Rikard Eide <rikardeide@gmail.com>
2022-07-24 23:21:04 +0200

    cabin-planner: add support storing new bookings remotely

---
+++ apps/cabin-planner/src/server/router/booking.ts
@@ -0,0 +1,25 @@
+import { bookingSchema_server } from '../forms/booking';
+import { createRouter } from './context';
+
+export const bookingRouter = createRouter()
+  .mutation('create', {
+    input: bookingSchema_server,
+    async resolve({ input, ctx }) {
+      await ctx.prisma.booking.create({
+        data: {
+          startDate: input.startDate,
+          endDate: input.endDate,
+          bedsRequired: input.bedCount,
+          description: input.description,
+          arrivalTime: input.arrivalTime,
+          user: { connect: { id: input.userId } },
+          property: { connect: { id: input.propertyId } },
+        },
+      });
+    },
+  })
+  .query('getAll', {
+    async resolve({ ctx }) {
+      return await ctx.prisma.booking.findMany();
+    },
+  });
