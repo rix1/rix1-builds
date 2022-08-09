e721a53078d698b4393266167ee56a5be1c09d95
Rikard Eide <rikardeide@gmail.com>
2022-08-09 21:38:24 +0200

    doggo: fix TS errors
    Co-authored-by: Siri Holtnæs <s.holtnaes@gmail.com>

--- apps/doggo/src/components/Navbar.tsx
+++ apps/doggo/src/components/Navbar.tsx
@@ -2,12 +2,22 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import clsx from "clsx";
+import { User, Users } from "../utils/eventTypes";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+type NavbarProps = {
+  currentUser?: User;
+  users: Users;
+  userIds: readonly string[];
+  onClick: (userId: string) => void;
+};
 
-export default function Navbar({ selectedUserId, users, userIds, onClick }) {
+export default function Navbar({
+  currentUser,
+  users,
+  userIds,
+  onClick,
+}: NavbarProps) {
   return (
     <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
@@ -45,11 +55,13 @@ export default function Navbar({ selectedUserId, users, userIds, onClick }) {
                   <div>
                     <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src={users[selectedUserId].image}
-                        alt=""
-                      />
+                      {currentUser && (
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src={currentUser.image}
+                          alt=""
+                        />
+                      )}
                     </Menu.Button>
                   </div>
                   <Transition
@@ -64,13 +76,16 @@ export default function Navbar({ selectedUserId, users, userIds, onClick }) {
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       {userIds.map((userId) => {
                         const user = users[userId];
+                        if (!user) {
+                          return null;
+                        }
                         return (
                           <Menu.Item key={user.id}>
                             {({ active }) => (
                               <button
                                 onClick={() => onClick(user.id)}
-                                className={classNames(
-                                  selectedUserId === user.id
+                                className={clsx(
+                                  currentUser?.id === user.id
                                     ? "bg-gray-100"
                                     : "",
                                   "flex px-4 py-2 text-sm text-gray-700 w-full text-left"
@@ -90,7 +105,7 @@ export default function Navbar({ selectedUserId, users, userIds, onClick }) {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
+                            className={clsx(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
--- apps/doggo/src/pages/index.tsx
+++ apps/doggo/src/pages/index.tsx
@@ -1,96 +1,40 @@
-import type { NextPage } from "next";
-import dayjs from "dayjs";
-import { nanoid } from "nanoid";
-import Head from "next/head";
-import { useState } from "react";
-import Navbar from "../components/Navbar";
+import dayjs from 'dayjs';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import {
+  Activity,
+  ChecklistItem,
+  Event,
+  TimeSlot,
+  Users,
+} from '../utils/eventTypes';
 
 function titleCase(word: string) {
   return `${word.slice(0, 1).toUpperCase()}${word.slice(1)}`;
 }
 
-type EventTypes = "walk" | "poop" | "food";
+const userIds = [
+  'cl639np4l0022kddy0m22vs6s',
+  'cl639np4r0030kddyoe0r6sfi',
+] as const;
 
-type User = {
-  id: String;
-  name: String;
-  email: String;
-  image: String;
-};
-
-type TimeSlot = "morning" | "lunch" | "dinner";
-type Activity = "walk" | "poop" | "food";
-type ChecklistItem = {
-  id: string;
-  timeslot: TimeSlot;
-  activity: Activity;
-  label: string;
-};
-
-function createChecklist(
-  timeslot: "morning" | "lunch" | "dinner"
-): ChecklistItem[] {
-  return [
-    {
-      id: nanoid(),
-      activity: "walk",
-      timeslot: timeslot,
-      label: "Walk",
-    },
-    {
-      id: nanoid(),
-      activity: "food",
-      timeslot: timeslot,
-      label: "Food",
-    },
-    {
-      id: nanoid(),
-      activity: "poop",
-      timeslot: timeslot,
-      label: "Poop",
-    },
-  ];
-}
-
-type Event = {
-  id: string;
-  createdAt: string;
-  createdBy: string;
-  activity: Activity;
-  timeslot: TimeSlot;
-};
-
-function createEvent(
-  activity: Activity,
-  timeslot: TimeSlot,
-  userId: string
-): Event {
-  return {
-    id: nanoid(),
-    createdAt: dayjs().toISOString(),
-    createdBy: userId,
-    activity: activity,
-    timeslot: timeslot,
-  };
-}
-
-const userIds = ["rikard", "siri"];
-
-const users = {
+const users: Users = {
   cl639np4l0022kddy0m22vs6s: {
-    id: "cl639np4l0022kddy0m22vs6s",
-    name: "Rikard",
-    email: "rikardeide@gmail.com",
+    id: 'cl639np4l0022kddy0m22vs6s',
+    name: 'Rikard',
+    email: 'rikardeide@gmail.com',
     image:
-      "https://s3.eu-north-1.amazonaws.com/rix1.dev/first-thursday-450x450.jpg",
+      'https://s3.eu-north-1.amazonaws.com/rix1.dev/first-thursday-450x450.jpg',
   },
   cl639np4r0030kddyoe0r6sfi: {
-    id: "cl639np4r0030kddyoe0r6sfi",
-    name: "Siri",
-    email: "s.holtnaes@gmail.com",
-    image: "https://avatars.githubusercontent.com/u/6916462?v=4",
+    id: 'cl639np4r0030kddyoe0r6sfi',
+    name: 'Siri',
+    email: 's.holtnaes@gmail.com',
+    image: 'https://avatars.githubusercontent.com/u/6916462?v=4',
   },
-};
+} as const;
 
 const eventState = {
   byId: {},
@@ -101,48 +45,72 @@ const Home: NextPage = () => {
   // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
   const [allEvents, setAllEvents] = useState<{ [key: string]: Event }>({});
   const [eventIds, setEventIds] = useState<string[]>([]);
-  const [currentUserId, setCurrentUserId] = useState<string>("siri");
+  const [currentUserId, setCurrentUserId] = useState<string>(userIds[0]);
+
+  function doesEventExist(eventId: string) {
+    console.log(eventIds, eventId);
+
+    return eventIds.includes(eventId);
+  }
+  function createChecklist(
+    timeslot: 'morning' | 'lunch' | 'dinner',
+  ): ChecklistItem[] {
+    return [
+      {
+        id: `${timeslot}-walk`,
+        activity: 'walk',
+        timeslot: timeslot,
+        label: 'Walk',
+      },
+      {
+        id: `${timeslot}-food`,
+        activity: 'food',
+        timeslot: timeslot,
+        label: 'Food',
+      },
+      {
+        id: `${timeslot}-poop`,
+        activity: 'poop',
+        timeslot: timeslot,
+        label: 'Poop',
+      },
+    ];
+  }
+
+  function createEvent(
+    activity: Activity,
+    timeslot: TimeSlot,
+    userId: string,
+  ): Event {
+    return {
+      id: `${activity}-${timeslot}-${userId}`,
+      createdAt: dayjs().toISOString(),
+      createdBy: userId,
+      activity: activity,
+      timeslot: timeslot,
+    };
+  }
 
   const timeslots = [
     {
-      id: "morning",
-      label: "Morning",
-      timeFrame: "07-12",
-      checklist: createChecklist("morning"),
+      id: 'morning',
+      label: 'Morning',
+      timeFrame: '07-12',
+      checklist: createChecklist('morning'),
     },
     {
-      id: "lunch",
-      label: "Lunch",
-      timeFrame: "12-17",
-      checklist: createChecklist("lunch"),
+      id: 'lunch',
+      label: 'Lunch',
+      timeFrame: '12-17',
+      checklist: createChecklist('lunch'),
     },
     {
-      id: "dinner",
-      label: "Dinner",
-      timeFrame: "17-23",
-      checklist: createChecklist("dinner"),
+      id: 'dinner',
+      label: 'Dinner',
+      timeFrame: '17-23',
+      checklist: createChecklist('dinner'),
     },
   ];
-
-  function doesEventExist(eventId: string) {
-    return eventIds.includes(eventId);
-  }
-
-  function addEvent(itemId: string) {
-    const newEvent = createEvent(itemId, currentUserId, ["lofot", "gata"]);
-    setEventIds((prev) => [...prev, newEvent.id]);
-    setAllEvents((prev) => ({
-      ...prev,
-      [newEvent.id]: newEvent,
-    }));
-  }
-
-  function removeEvent(itemId: string) {
-    setEventIds((prev) => prev.filter((id) => id !== itemId));
-    const events = allEvents;
-    delete events[itemId];
-    setAllEvents(events);
-  }
 
   return (
     <>
@@ -153,7 +121,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Navbar
-        selectedUserId={currentUserId}
+        currentUser={users[currentUserId]}
         users={users}
         userIds={userIds}
         onClick={(userId) => setCurrentUserId(userId)}
@@ -161,13 +129,13 @@ const Home: NextPage = () => {
 
       <main className="container mx-auto flex flex-col min-h-screen p-4">
         <h1 className="text-3xl font-bold border-l-4 pl-4 border-green-400 mt-4">
-          {dayjs().format("ddd, D[th] MMMM, YYYY")}
+          {dayjs().format('ddd, D[th] MMMM, YYYY')}
         </h1>
         <div className="space-y-10 sm:space-y-0 space-x-0 sm:space-x-10 pt-8 flex flex-col sm:flex-row">
           {timeslots.map((timeslot) => (
             <section className="" key={timeslot.id}>
               <h2 className="text-xl font-bold inline-block">
-                {timeslot.label}{" "}
+                {timeslot.label}{' '}
                 <span className="ml-1 font-normal text-base text-gray-400">
                   ({timeslot.timeFrame})
                 </span>
@@ -182,9 +150,23 @@ const Home: NextPage = () => {
                       type="checkbox"
                       onClick={() => {
                         if (doesEventExist(item.id)) {
-                          removeEvent(item.id);
+                          setEventIds((prev) =>
+                            prev.filter((id) => id !== item.id),
+                          );
+                          const events = allEvents;
+                          delete events[item.id];
+                          setAllEvents(events);
                         } else {
-                          addEvent(item.id);
+                          const newEvent = createEvent(
+                            item.activity,
+                            item.timeslot,
+                            currentUserId,
+                          );
+                          setEventIds((prev) => [...prev, newEvent.id]);
+                          setAllEvents((prev) => ({
+                            ...prev,
+                            [newEvent.id]: newEvent,
+                          }));
                         }
                       }}
                       className="mr-2"
@@ -205,19 +187,23 @@ const Home: NextPage = () => {
             <ul className="pt-4 flex flex-col">
               {eventIds.map((eventId) => {
                 const event = allEvents[eventId];
+                const user = users[event?.createdBy || ''];
+                if (!event || !user) {
+                  return null;
+                }
                 return (
                   <li key={eventId} className="flex-auto space-x-4 my-2">
                     <div className="flex">
                       <img
-                        src={users[event.createdBy].image}
+                        src={user.image}
                         className="w-7 h-7 rounded-full"
-                        alt={users[event.createdBy].name}
+                        alt={user.name}
                       />
                       <span className="ml-3">
-                        {titleCase(event.timeslot)} {event.eventType}
+                        {titleCase(event.timeslot)} {event.activity}
                       </span>
                       <div className="tabular-nums ml-auto justify-self-center text-gray-500">
-                        {dayjs(event.createdAt).format("hh:mm:ss")}
+                        {dayjs(event.createdAt).format('hh:mm:ss')}
                       </div>
                     </div>
                   </li>
