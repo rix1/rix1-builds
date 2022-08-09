5f2781c1cbfd5efdea8a4ec211afd71211fc8e81
Rikard Eide <rikardeide@gmail.com>
2022-08-09 21:40:56 +0200

    doggo: add empty Prisma seed script
    Intentionally, this doesn't do anything right now.
    
    Co-authored-by: Siri Holtnæs <s.holtnaes@gmail.com>

---
+++ apps/doggo/src/server/db/seed.ts
@@ -0,0 +1,19 @@
+const users = [
+  {
+    id: "cl639np4l0022kddy0m22vs6s",
+    name: "Rikard",
+    email: "rikardeide@gmail.com",
+    image:
+      "https://s3.eu-north-1.amazonaws.com/rix1.dev/first-thursday-450x450.jpg",
+  },
+  {
+    id: "cl639np4r0030kddyoe0r6sfi",
+    name: "Siri",
+    email: "s.holtnaes@gmail.com",
+    image: "https://avatars.githubusercontent.com/u/6916462?v=4",
+  },
+];
+
+console.error('❌ No seed script is defined yet!')
+
+export {};
