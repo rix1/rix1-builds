a445b8f5ad9784f499a22ec9d65fa0788ce294b8
Rikard Eide <rikardeide@gmail.com>
2022-08-09 21:40:40 +0200

    doggo: set up eslint, prettier and tailwind to use shared config
    Co-authored-by: Siri Holtnæs <s.holtnaes@gmail.com>

--- apps/doggo/tailwind.config.cjs
+++ apps/doggo/tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  // content is handled by the shared config!
+  // content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  presets: [require('config/tailwind.config')],
   theme: {
     extend: {},
   },
