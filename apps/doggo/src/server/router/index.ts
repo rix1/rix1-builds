68dd589c7851e23b53452590236ec1ce015b84cb
Rikard Eide <rikardeide@gmail.com>
2022-08-16 11:04:45 +0200

    doggo: expose events with tRPC

--- apps/doggo/src/server/router/index.ts
+++ apps/doggo/src/server/router/index.ts
@@ -1,14 +1,14 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
-import superjson from "superjson";
+import { createRouter } from './context';
+import superjson from 'superjson';
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
+import { protectedExampleRouter } from './protected-example-router';
+import { eventsRouter } from './eventsRouter';
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("question.", protectedExampleRouter);
+  .merge('dogEvents.', eventsRouter)
+  .merge('question.', protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
