9acafa9a643f8d56b9e4ab07da9fdfb807894b11
Rikard Eide <rikardeide@gmail.com>
2023-02-05 07:33:52 +0100

    luxury-buttons: complete hover-effect

--- apps/luxury-buttons/twind.config.ts
+++ apps/luxury-buttons/twind.config.ts
@@ -2,4 +2,22 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    fontFamily: {
+      janson: "Janson Text",
+    },
+  },
+  preflight: {
+    // Import external stylesheet
+    // "@import":
+    //   `url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap')`,
+    // Declare font face
+    "@font-face": [
+      {
+        fontFamily: "Janson Text",
+        fontWeight: "400",
+        src: 'url(/janson.woff2) format("woff")',
+      },
+    ],
+  },
 } as Options;
