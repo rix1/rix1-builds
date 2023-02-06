67bc2fd4de0685cdb863e940909fd66d96025b87
Rikard Eide <rikardeide@gmail.com>
2023-02-06 09:09:27 +0100

    css-experiments: add retro menu thingy
    Inspired by a thing on the internet [1]
    
    [1]: https://twitter.com/AisleOne/status/1621907880180912130

--- apps/css-experiments/twind.config.ts
+++ apps/css-experiments/twind.config.ts
@@ -3,20 +3,30 @@ import { Options } from "$fresh/plugins/twind.ts";
 export default {
   selfURL: import.meta.url,
   theme: {
-    fontFamily: {
-      janson: "Janson Text",
+    extend: {
+      fontFamily: {
+        janson: "Janson Text",
+        comfortaa: "Comfortaa",
+        pressStart: `'Press Start 2P', cursive`,
+      },
     },
   },
   preflight: {
     // Import external stylesheet
-    // "@import":
-    //   `url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap')`,
+    "@import":
+      `url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');`,
+
     // Declare font face
     "@font-face": [
       {
         fontFamily: "Janson Text",
         fontWeight: "400",
         src: 'url(/janson.woff2) format("woff")',
+      },
+      {
+        fontFamily: "Comfortaa",
+        fontWeight: "700",
+        src: 'url(/Comfortaa-Bold.ttf) format("ttf")',
       },
     ],
   },
