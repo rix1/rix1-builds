60f25a4c04f22275d13781aab6dd33144677af21
Rikard Eide <rikardeide@gmail.com>
2023-07-28 17:53:36 +0200

    pizza: support adjusting pie size (hidden feature)

--- apps/pizza/islands/Slider.tsx
+++ apps/pizza/islands/Slider.tsx
@@ -1,4 +1,28 @@
-import { slider, SLIDER_MAX, SLIDER_MIN } from "../lib/recipe.ts";
+import { useState } from "preact/hooks";
+import { pieSize, slider, SLIDER_MAX, SLIDER_MIN } from "../lib/recipe.ts";
+
+const PieSize = () => {
+  const [isLocked, setIsLocked] = useState(true);
+
+  if (isLocked) {
+    return (
+      <span className="tabular-nums" onClick={() => setIsLocked(false)}>
+        {pieSize.value}g
+      </span>
+    );
+  }
+  return (
+    <>
+      <input
+        value={pieSize.value}
+        className="max-w-[48px] focus:bg-gray-50 mr-1"
+        onChange={(e) => pieSize.value = Number(e.currentTarget.value)}
+        onBlur={() => setIsLocked(true)}
+        type="number"
+      />g
+    </>
+  );
+};
 
 const Slider = () => {
   return (
@@ -6,7 +30,7 @@ const Slider = () => {
       htmlFor="pizza-slider"
       className="max-w-lg block mt-4 text-gray-500 text-sm"
     >
-      Adjust number of 260g pies (<output>{slider.value}</output>){" "}
+      Adjust number of pies (<output>{slider.value}</output> x <PieSize />){" "}
       <input
         name="pizza-slider"
         id="pizza-slider"
