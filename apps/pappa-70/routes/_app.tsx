776f563c6f9c0461d13aa2ac1fed123af8996dd0
Rikard Eide <rikardeide@gmail.com>
2023-09-04 00:22:58 +0200

    pappa-70: remove details arrows from Safari

--- apps/pappa-70/routes/_app.tsx
+++ apps/pappa-70/routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/globals.css" />
         <title>pappa-70</title>
       </head>
       <body>
