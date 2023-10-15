e0afdd406cd6dd19c7f5ee1c36e687dbb7526d83
Rikard Eide <rikardeide@gmail.com>
2023-10-15 09:11:20 +0200

    bs-cli: make proof of concept on a plane
    only outputs in the terminal.

--- apps/bs-cli/main.ts
+++ apps/bs-cli/main.ts
@@ -1,8 +1,41 @@
-export function add(a: number, b: number): number {
-  return a + b;
+let ts = performance.now();
+let currentBeat = 0;
+
+const bpm = 178;
+const msBetweenBeats = (1 / (bpm / 60)) * 1000;
+
+function tick(cb: (beat: number) => void) {
+  if (performance.now() - ts >= msBetweenBeats) {
+    currentBeat += 1;
+    ts = performance.now();
+    cb(currentBeat);
+  }
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+function createBar(currentBeat) {
+  return Array.from({ length: 8 })
+    .map((el, index) => {
+      const isActive = currentBeat % 8 === index;
+      if (isActive) {
+        if ([2, 6].includes(index)) {
+          return "🥁";
+        }
+        if ([1, 3, 5, 7].includes(index)) {
+          return "👏";
+        }
+      }
+      return "_";
+    })
+    .toString();
+}
+
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  while (true) {
+    tick((currentBeat) => {
+      if (currentBeat % 8 === 0) {
+        console.log();
+      }
+      console.log(createBar(currentBeat));
+    });
+  }
 }
