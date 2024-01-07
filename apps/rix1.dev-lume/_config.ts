1d9fb2cbdc0557257ef152377dff45cd9e1d0bc6
Rikard Eide <rikardeide@gmail.com>
2024-01-07 15:13:47 +0100

    rix1.dev-lume: initial commit

---
+++ apps/rix1.dev-lume/_config.ts
@@ -0,0 +1,33 @@
+import lume from "lume/mod.ts";
+import code_highlight from "lume/plugins/code_highlight.ts";
+import date from "lume/plugins/date.ts";
+import favicon from "lume/plugins/favicon.ts";
+import feed from "lume/plugins/feed.ts";
+import lightningcss from "lume/plugins/lightningcss.ts";
+import reading_info from "lume/plugins/reading_info.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import postcss from "lume/plugins/postcss.ts";
+import typography from "npm:@tailwindcss/typography";
+import jsx from "lume/plugins/jsx.ts";
+
+const site = lume();
+
+site.use(code_highlight());
+site.use(date());
+site.use(favicon());
+site.use(feed());
+site.use(lightningcss());
+site.use(reading_info());
+site.use(sitemap());
+site.use(jsx());
+site.use(
+  tailwindcss({
+    options: {
+      plugins: [typography],
+    },
+  })
+);
+site.use(postcss());
+
+export default site;
