c1be2e1ca5e3b5d37b18b85ffa5b85f74a90c7e8
Rikard Eide <rikardeide@gmail.com>
2024-01-30 00:19:11 +0100

    rix1dotdev: add www redirect

--- apps/rix1.dev-lume/serve.ts
+++ apps/rix1.dev-lume/serve.ts
@@ -5,6 +5,22 @@ const server = new Server({
   root: `${Deno.cwd()}/_site`,
 });
 
+server.use(async (request, next) => {
+  const url = new URL(request.url);
+
+  if (url.hostname === "rix1.dev") {
+    return new Response(null, {
+      status: 307,
+      headers: {
+        Location: `https://www.rix1.dev${url.pathname}${url.search}`,
+      },
+    });
+  }
+
+  const response = await next(request);
+  return response;
+});
+
 server.start();
 
 console.log("Listening on http://localhost:8000");
